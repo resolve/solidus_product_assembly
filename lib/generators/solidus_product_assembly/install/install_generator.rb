e5293f48a22e5d15172611ea5528d48c50d67fdf
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-02-01 11:06:12 +1300

    Further Spree -> Solidus namespace changes

--- lib/generators/solidus_product_assembly/install/install_generator.rb
+++ lib/generators/solidus_product_assembly/install/install_generator.rb
@@ -1,4 +1,4 @@
-module SpreeProductAssembly
+module SolidusProductAssembly
   module Generators
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, :type => :boolean, :default => false
